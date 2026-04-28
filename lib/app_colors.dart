87492f15429b838d6353c3399b0883fb41a5396e
Achmad Raffa Adhiyaksa <raffaadhiyaksa2@gmail.com>
2026-04-28 22:43:12 +0700

    feat: collaboration screen

---
+++ lib/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color surface = Color(0xFFF5F5F5);
+  static const Color card = Color(0xFFFFFFFF);
+  static const Color primary = Color(0xFF22C55E);
+  static const Color primaryLight = Color(0x2022C55E);
+  static const Color textPrimary = Color(0xFF111111);
+  static const Color textSecondary = Color(0xFF555555);
+  static const Color textMuted = Color(0xFF9CA3AF);
+  static const Color border = Color(0xFFE5E7EB);
+  static const Color chipBg = Color(0xFFF3F4F6);
+  static const Color searchBg = Color(0xFFF3F4F6);
+
+  static const List<Color> avatarColors = [
+    Color(0xFF7C3AED),
+    Color(0xFFEC4899),
+    Color(0xFFF59E0B),
+    Color(0xFF10B981),
+    Color(0xFF3B82F6),
+  ];
+}
